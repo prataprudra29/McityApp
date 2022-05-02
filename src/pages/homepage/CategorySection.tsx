89d66543556d4f3c3c9a9acf877b763d729930eb
Rudra Pratap Singh <rudrapratapsingh@Rudras-MacBook-Pro.local>
2022-05-02 17:30:20 +0530

    dynamic data changes

--- src/pages/homepage/CategorySection.tsx
+++ src/pages/homepage/CategorySection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useTypedSelector } from "../../store/typeSelector";
 import { getStoreData, sideBarClickData } from "../../store/account/action";
@@ -6,49 +6,56 @@ import { getStoreData, sideBarClickData } from "../../store/account/action";
 const CategorySection = () => {
   let myState: any = {
     category: false,
-    categories: []
-  }
+    categories: [],
+  };
   const [state, setState] = useState(myState);
   const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch<any>(getStoreData());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const data: any = useTypedSelector((state) => state.storeReducer);
   console.log(data);
 
   const openCategory = (data: any) => {
     setState({ category: !state.category });
     dispatch<any>(sideBarClickData(data));
-  }
+  };
 
-  React.useEffect(() => {
-    dispatch<any>(getStoreData());
+  const categoryData = useMemo(() => {
     if (data) {
-      setState({ ...state, categories: data.storeData.categories })
+      setState({ ...state, categories: data?.storeData?.categories });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [data]);
+  console.log("state", state);
 
   return (
     <>
-      <div className={`leftsidebar ${state.category ? 'showCategory' : null}`}>
+      <div className={`leftsidebar ${state.category ? "showCategory" : null}`}>
         <ul className="m-0 p-0 w-100">
-          {
-            state.categories.map((res: any) => (
-              <li className="categoryName" key={res.name}>{res.name} <span className="countName">{res.count}</span></li>
-            ))
-          }
+          {state?.categories &&
+            state?.categories.map((res: any) => (
+              <li className="categoryName" key={res.name}>
+                {res.name} <span className="countName">{res.count}</span>
+              </li>
+            ))}
         </ul>
         <div className="w-100 viewAllCategorios">
           View All Categories <i className="fa fa-arrow-right"></i>
         </div>
       </div>
 
-      {!state.category &&
+      {!state.category && (
         <div className="categoryButton" onClick={openCategory}>
           <div className="catButton">
             <i className="fa fa-th-large"></i>
           </div>
         </div>
-      }
-      <div className={`overlayleft ${state.category ? 'overlayShow' : null}`} onClick={openCategory}></div>
+      )}
+      <div
+        className={`overlayleft ${state.category ? "overlayShow" : null}`}
+        onClick={openCategory}
+      ></div>
     </>
   );
 };
